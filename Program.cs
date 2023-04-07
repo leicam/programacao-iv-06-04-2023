56519953de899d3ebc54f414fa724645abb56b31
Juliano Ribeiro de Souza Maciel <juliano.maciel@faculdadefacec.edu.br>
2023-04-06 21:08:44 -0300

    Implementações iniciais

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Revisao.Primeiro.Trimestre
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
